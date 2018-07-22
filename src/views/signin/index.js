31e7df4ead417cb89b70f2d4db5cd1d4619f436e
WilliamCui31 <444738577@qq.com>
2018-07-22 23:03:57 +0800

    优化登录注册样式

--- src/views/signin/index.js
+++ src/views/signin/index.js
@@ -7,175 +7,174 @@ import { JSEncrypt } from '../../utils/jsencrypt.js';
 import { PUBLI_KEY } from '../../utils/constants';
 
 class SignIn extends Component {
-    state = {
-        username: '',
-        password: '',
-        imgName: '',
-        errorTip: '',
-        code: '',
-        url: ''
-    };
-    request = window.request;
-    inputValue = e => {
-        this.setState({ [e.target.id]: e.target.value });
-    };
+  state = {
+    username: '',
+    password: '',
+    imgName: '',
+    errorTip: '',
+    code: '',
+    url: ''
+  };
+  request = window.request;
+  inputValue = e => {
+    this.setState({ [e.target.id]: e.target.value });
+  };
 
-    handleSubmit = () => {
-        const { username, password, code } = this.state;
-        let encrypt = new JSEncrypt();
-        encrypt.setPublicKey(PUBLI_KEY);
-        let enPassword = encrypt.encrypt(password);
+  handleSubmit = () => {
+    const { username, password, code } = this.state;
+    let encrypt = new JSEncrypt();
+    encrypt.setPublicKey(PUBLI_KEY);
+    let enPassword = encrypt.encrypt(password);
 
-        if (username && password) {
-            this.request('/user/login', {
-                body: {
-                    username,
-                    password: enPassword,
-                    code
-                }
-            }).then(json => {
-                if (json.code === 10000000) {
-                    sessionStorage.setItem('account', JSON.stringify(json.data));
-                    this.props.history.push('/trade');
-                } else if(json.code === 10001001) {
-                    this.getValidImg();
-                    this.setState({ errorTip: json.msg });
-                }else if(json.code === 10001000){
-                    this.setState({ errorTip: '用户名或密码不正确' });
-                }else{
-                    this.setState({ errorTip: json.msg });
-                }
-            });
+    if (username && password) {
+      this.request('/user/login', {
+        body: {
+          username,
+          password: enPassword,
+          code
         }
-    };
-
-    //获取访问的网址
-    getUrl = () => {
-        this.request('/cms/link', {
-            method: 'GET',
-        }).then(json => {
-            if (json.code === 10000000) {
-                this.setState({ url: json.data });
-            } else {
-                message.error(json.msg);
-            }
-        });
+      }).then(json => {
+        if (json.code === 10000000) {
+          sessionStorage.setItem('account', JSON.stringify(json.data));
+          this.props.history.push('/trade');
+        } else if (json.code === 10001001) {
+          this.getValidImg();
+          this.setState({ errorTip: json.msg });
+        } else if (json.code === 10001000) {
+          this.setState({ errorTip: '用户名或密码不正确' });
+        } else {
+          this.setState({ errorTip: json.msg });
+        }
+      });
     }
-    componentWillMount(){
-        this.getUrl();
-    }
+  };
 
-    getValidImg = () => {
-        const { username } = this.state;
-        this.request('/valid/createCode', {
-            method: 'GET',
-            body: {
-                username,
-                type: 'login'
-            }
-        }).then(json => {
-            if (json.code === 10000000) {
-                this.setState({ imgName: json.data.imageName });
-            } else {
-                message.error(json.msg);
-            }
-        });
-    };
+  //获取访问的网址
+  getUrl = () => {
+    this.request('/cms/link', {
+      method: 'GET'
+    }).then(json => {
+      if (json.code === 10000000) {
+        this.setState({ url: json.data });
+      } else {
+        message.error(json.msg);
+      }
+    });
+  };
+  componentWillMount() {
+    this.getUrl();
+  }
 
-    render() {
-        const { username, password, errorTip, code, imgName, url } = this.state;
-        const ok = this.state.username && this.state.password;
-        return (
-            <div className="content">
-                <div className="form-box">
-                    <h1>用户登录</h1>
-                    <div className="attention">
-                        <i className="iconfont icon-zhuyishixiang" />请确认您正在访问{' '}
-                        <strong>{url}</strong>
-                    </div>
-                    <div className="safety-site">
-                        <i className="iconfont icon-suo1" />
-                        {url}
-                    </div>
-                    <p className="error-tip">
-                        {errorTip && <i className="iconfont icon-zhuyishixiang" />}
-                        {errorTip}
-                    </p>
-                    <ul className="form-list">
-                        <li>
-                            <i className="iconfont icon-yonghu1" />
-                            <input
-                                type="text"
-                                className="text"
-                                id="username"
-                                value={username}
-                                onChange={this.inputValue}
-                                placeholder="账号"
-                            />
-                        </li>
-                        <li>
-                            <i className="iconfont icon-suo" />
-                            <input
-                                type="password"
-                                className="text"
-                                id="password"
-                                value={password}
-                                onChange={this.inputValue}
-                                placeholder="密码"
-                                onKeyPress={e => {
-                                    if (e.which === 13) this.handleSubmit();
-                                }}
-                            />
-                        </li>
-                        {imgName && [
-                            <li key="code">
-                                <i className="iconfont icon-yanzhengma2" />
-                                <input
-                                    type="text"
-                                    className="text"
-                                    id="code"
-                                    value={code}
-                                    onChange={this.inputValue}
-                                    placeholder="验证码"
-                                    onKeyPress={e => {
-                                        if (e.which === 13) this.handleSubmit();
-                                    }}
-                                />
-                                <img
-                                    src={`${IMAGES_ADDRESS}/image/view/${imgName}`}
-                                    className="inner-graphic"
-                                    alt="图形验证码"
-                                    onClick={this.getValidImg}
-                                />
-                            </li>,
-                            <li key="tips" style={{ textAlign: 'right' }}>
-                                点击图片刷新验证码
-                            </li>
-                        ]}
-                        <li>
-                            <input
-                                type="submit"
-                                className={classnames({
-                                    button: true,
-                                    disabled: !ok
-                                })}
-                                onClick={this.handleSubmit}
-                                value="登录"
-                            />
-                        </li>
-                        <li className="clear">
-                            <Link to="/reset" className="pull-left">
-                                忘记密码？
-                            </Link>{' '}
-                            <span className="pull-right">
-                                还没账号？<Link to="/signup">立即注册</Link>
-                            </span>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        );
-    }
+  getValidImg = () => {
+    const { username } = this.state;
+    this.request('/valid/createCode', {
+      method: 'GET',
+      body: {
+        username,
+        type: 'login'
+      }
+    }).then(json => {
+      if (json.code === 10000000) {
+        this.setState({ imgName: json.data.imageName });
+      } else {
+        message.error(json.msg);
+      }
+    });
+  };
+
+  render() {
+    const { username, password, errorTip, code, imgName, url } = this.state;
+    const ok = this.state.username && this.state.password;
+    return (
+      <div className="content">
+        <div className="form-box">
+          <h1>用户登录</h1>
+          <div className="attention">
+            <i className="iconfont icon-zhuyishixiang" style={{color: '#C31823'}}/>请确认您正在访问 <strong>{url}</strong>
+          </div>
+          <div className="safety-site">
+            <i className="iconfont icon-suo1" style={{ color: '#53B135' }} />
+            {url}
+          </div>
+          <p className="error-tip">
+            {errorTip && <i className="iconfont icon-zhuyishixiang" />}
+            {errorTip}
+          </p>
+          <ul className="form-list">
+            <li>
+              <i className="iconfont icon-yonghu1" />
+              <input
+                type="text"
+                className="text"
+                id="username"
+                value={username}
+                onChange={this.inputValue}
+                placeholder="账号"
+              />
+            </li>
+            <li>
+              <i className="iconfont icon-suo" />
+              <input
+                type="password"
+                className="text"
+                id="password"
+                value={password}
+                onChange={this.inputValue}
+                placeholder="密码"
+                onKeyPress={e => {
+                  if (e.which === 13) this.handleSubmit();
+                }}
+              />
+            </li>
+            {imgName && [
+              <li key="code">
+                <i className="iconfont icon-yanzhengma2" />
+                <input
+                  type="text"
+                  className="text"
+                  id="code"
+                  value={code}
+                  onChange={this.inputValue}
+                  placeholder="验证码"
+                  onKeyPress={e => {
+                    if (e.which === 13) this.handleSubmit();
+                  }}
+                />
+                <img
+                  src={`${IMAGES_ADDRESS}/image/view/${imgName}`}
+                  className="inner-graphic"
+                  alt="图形验证码"
+                  onClick={this.getValidImg}
+                />
+              </li>,
+              <li key="tips" style={{ textAlign: 'right' }}>
+                点击图片刷新验证码
+              </li>
+            ]}
+            <li>
+              <input
+                type="submit"
+                className={classnames({
+                  button: true,
+                  disabled: !ok
+                })}
+                onClick={this.handleSubmit}
+                value="登录"
+              />
+            </li>
+            <li className="clear">
+              <Link to="/reset" className="pull-left">
+                忘记密码？
+              </Link>{' '}
+              <span className="pull-right">
+                还没账号？<Link to="/signup">立即注册</Link>
+              </span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default SignIn;
