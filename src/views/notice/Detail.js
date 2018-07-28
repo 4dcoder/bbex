257b79eda8c5e3089ba190ec8b258e13c97e18f2
WilliamCui31 <444738577@qq.com>
2018-07-28 18:21:11 +0800

    优化请求完善案背景

--- src/views/notice/Detail.js
+++ src/views/notice/Detail.js
@@ -3,38 +3,52 @@ import { message } from 'antd';
 import './detail.css';
 
 class Detail extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            content: '<p></p>'
-        };
-    }
-    request = window.request;
-    
-    componentWillMount() {
-        this.getNoticeDetail(this.props.match.params.id);
-    }
-
-    //获取公告
-    getNoticeDetail = id => {
-        this.request('/cms/view/' + id, {
-            method: 'GET'
-        }).then(json => {
-            if (json.code === 10000000) {
-                this.setState({ content: json.data.content });
-            } else {
-                message.error(json.msg);
-            }
-        });
+  constructor(props) {
+    super(props);
+    this.state = {
+      content: '<p></p>'
     };
+  }
 
-    render() {
-        return (
-            <div className="content-inner notice-detail">
-                <div dangerouslySetInnerHTML={{ __html: this.state.content }} />
-            </div>
-        );
-    }
+  request = window.request;
+
+  container = document.querySelector('.container');
+
+  componentWillMount() {
+    this.getNoticeDetail(this.props.match.params.id);
+  }
+
+  componentDidMount() {
+    // 给container 添加 html-wrap
+    const classname = this.container.className;
+    this.container.className = classname + ' html-wrap';
+  }
+
+  componentWillUnmount() {
+    // 给container 移除 html-wrap
+    this.container.className = 'container';
+  }
+
+  //获取公告
+  getNoticeDetail = id => {
+    this.request('/cms/view/' + id, {
+      method: 'GET'
+    }).then(json => {
+      if (json.code === 10000000) {
+        this.setState({ content: json.data.content });
+      } else {
+        message.error(json.msg);
+      }
+    });
+  };
+
+  render() {
+    return (
+      <div className="content-inner notice-detail">
+        <div dangerouslySetInnerHTML={{ __html: this.state.content }} />
+      </div>
+    );
+  }
 }
 
 export default Detail;
