032415fe98cec67bfb01ce507dff39812fc2d269
WilliamCui31 <444738577@qq.com>
2018-08-01 16:47:03 +0800

    优化404页面

--- src/App.js
+++ src/App.js
@@ -104,19 +104,19 @@ class App extends Component {
           <Container {...{ localization }} onGetLocalization={this.handleGetLocalization}>
             <Switch>
               <NormalRoute exact path="/" component={Home} {...{ localization }} />
-              <NormalRoute path="/trade" component={Trade} {...{ localization }} />
-              <NormalRoute path="/signin" component={SignIn} {...{ localization }} />
-              <NormalRoute path="/signup" component={SignUp} {...{ localization }} />
-              <NormalRoute path="/reset" component={Reset} {...{ localization }} />
-              <NormalRoute path="/resetPassword" component={ResetPassword} {...{ localization }} />
-              <PrivateRoute path="/user" component={User} {...{ localization }} />
-              <NormalRoute path="/c2c" component={C2c} {...{ localization }} />
-              <NormalRoute path="/link/:id" component={MyLink} {...{ localization }} />
-              <NormalRoute path="/agreement" component={Agreement} {...{ localization }} />
+              <NormalRoute exact path="/trade" component={Trade} {...{ localization }} />
+              <NormalRoute exact path="/signin" component={SignIn} {...{ localization }} />
+              <NormalRoute exact path="/signup" component={SignUp} {...{ localization }} />
+              <NormalRoute exact path="/reset" component={Reset} {...{ localization }} />
+              <NormalRoute exact path="/resetPassword" component={ResetPassword} {...{ localization }} />
+              <PrivateRoute exact path="/user" component={User} {...{ localization }} />
+              <NormalRoute exact path="/c2c" component={C2c} {...{ localization }} />
+              <NormalRoute exact path="/link/:id" component={MyLink} {...{ localization }} />
+              <NormalRoute exact path="/agreement" component={Agreement} {...{ localization }} />
               <NormalRoute exact path="/notice" component={Notice} {...{ localization }} />
-              <NormalRoute path="/notice/:id" component={NoticeDetail} {...{ localization }} />
+              <NormalRoute exact path="/notice/:id" component={NoticeDetail} {...{ localization }} />
               <NormalRoute exact path="/help" component={Help} {...{ localization }} />
-              <NormalRoute path="/help/:id" component={HelpDetail} {...{ localization }} />
+              <NormalRoute exact path="/help/:id" component={HelpDetail} {...{ localization }} />
               <NormalRoute component={NotFound} {...{ localization }} />
             </Switch>
           </Container>
--- src/views/404/index.js
+++ src/views/404/index.js
@@ -3,19 +3,19 @@ import { Button } from 'antd';
 import './404.css';
 
 class NotFound extends Component {
-
   handleToIndex = () => {
     this.props.history.push('/');
-  }
+  };
   render() {
     return (
-     <div className="page-404">
-        <div>
-          <h1>未找到该页面</h1>
-          <Button type='primary' onClick={this.handleToIndex} style={{borderRadius:4, width: 140, height: 40 }}>回到首页</Button>
-        </div>
-     </div>
-    )
+      <div className="content page-404">
+        <div className="banner">404</div>
+        <h3 className>对不起，未找到该页面。</h3>
+        <Button type="normal" onClick={this.handleToIndex} style={{ width: 140, height: 40 }}>
+          回到首页
+        </Button>
+      </div>
+    );
   }
 }
 
